a31cbe76a92a919278aea53feb5dff4c53347272
Mark Naylor <marknaylor@MacBookPro.attlocal.net>
2025-07-02 11:01:48 -0500

    chore(update checker): remove update checker

--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors:      The University of Tennessee, Knoxville
 Tags:              test
 Tested up to:      6.7
-Stable tag:        1.0.5
+Stable tag:        1.0.7
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
--- updateable-plugin.php
+++ updateable-plugin.php
@@ -2,36 +2,39 @@
 /**
  * Plugin Name: Updateable Plugin
  * Description: A simple plugin for testing updates.
- * Version: 1.0.6
+ * Version: 1.0.7
  * Author: Mark
  * License: GPL2
+ *
+ * @package UpdateablePlugin
  */
 
-// Include plugin update checker
-require_once plugin_dir_path( __FILE__ ) . 'plugin-update-checker.php';
+// Add a menu item to the admin dashboard.
+add_action( 'admin_menu', 'updateable_plugin_add_admin_menu' );
 
-
-// Add a menu item to the admin dashboard
-add_action('admin_menu', 'updateable_plugin_add_admin_menu');
-
+/**
+ * Adds menu page for testing
+ */
 function updateable_plugin_add_admin_menu() {
 	add_menu_page(
-		'Updateable Plugin',           // Page title
-		'Updateable Plugin',           // Menu title
-		'manage_options',              // Capability
-		'updateable-plugin',           // Menu slug
-		'updateable_plugin_admin_page',// Callback function
-		'dashicons-update',            // Icon
-		80                             // Position
+		'Updateable Plugin',           // Page title.
+		'Updateable Plugin',           // Menu title.
+		'manage_options',              // Capability.
+		'updateable-plugin',           // Menu slug.
+		'updateable_plugin_admin_page', // Callback function.
+		'dashicons-update',            // Icon.
+		80                             // Position.
 	);
 }
 
 
-// Admin page content
+/**
+ * Menu page content
+ */
 function updateable_plugin_admin_page() {
 	?>
 	<div class="wrap">
-		<h1>Updateable Plugin (Updated 6/27/25)</h1>
+		<h1>Updateable Plugin (Updated 7/2/25)</h1>
 		<p>This is a test plugin.</p>
 	</div>
 	<?php
